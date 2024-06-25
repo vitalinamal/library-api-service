8ad0c821e6fdaeaa8ab59e71e000a126b17d9ef4
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-06-25 11:07:33 +0200

    move constant FINE_MULTIPLIER on the top

--- payment/payment_calculator.py
+++ payment/payment_calculator.py
@@ -2,6 +2,8 @@ from decimal import Decimal
 
 from borrowing.models import Borrowing
 
+FINE_MULTIPLIER = 2
+
 
 def calculate_total_price(borrowing: Borrowing) -> Decimal:
     """
@@ -17,9 +19,8 @@ def calculate_fine(borrowing: Borrowing) -> Decimal:
     """
     Calculate the fine for overdue borrowing of a book.
     """
-    fine_multiplier = 2
     days_overdue = (borrowing.actual_return_date
                     - borrowing.expected_return_date).days
     daily_fee = borrowing.book.daily_fee
-    fine_amount = days_overdue * daily_fee * fine_multiplier
+    fine_amount = days_overdue * daily_fee * FINE_MULTIPLIER
     return Decimal(fine_amount)
